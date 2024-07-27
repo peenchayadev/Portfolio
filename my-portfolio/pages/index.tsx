e802a24906a82ec3c2b168a8cb9fbfa6b38cc0ed
Peenchayakorn Tanakornpornsawas <148639119+peenchayadev@users.noreply.github.com>
2024-07-27 20:10:17 +0700

    new project

---
+++ my-portfolio/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { HomePage } from '@/presentation/home/pages'
+
+const HomeRoute = () => {
+  //---------------------
+  //   RENDER
+  //---------------------
+  return <HomePage />
+}
+
+export default HomeRoute
---
+++ my-portfolio/src/presentation/home/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const HomePage = () => {
+  //---------------------
+  //   RENDER
+  //---------------------
+  return <div className="text-red-500">sss</div>
+}
