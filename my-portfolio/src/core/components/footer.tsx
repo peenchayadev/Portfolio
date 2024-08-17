ba5e20ad0ee8fc9a798c9c4f99c8d1e18157517f
Peenchayakorn Tanakornpornsawas <148639119+peenchayadev@users.noreply.github.com>
2024-08-17 10:42:19 +0700

    c

--- my-portfolio/src/core/components/footer.tsx
+++ my-portfolio/src/core/components/footer.tsx
@@ -4,9 +4,5 @@ export const Footer = () => {
   //---------------------
   //   RENDER
   //---------------------
-  return (
-    <div className="flex justify-center items-center mb-[100px]">
-      <div className="textA font-medium">Copyright @2024 | Designed by Peenchayakorn Tanakornpornsawas</div>
-    </div>
-  )
+  return <div></div>
 }
